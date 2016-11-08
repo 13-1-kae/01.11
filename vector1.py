ba28d848b57dfb1c46970e074b6ed8fb42e05b63
giorgio <playclevergames@mail.ru>
2016-11-08 12:11:28 +0300

    1 done

---
+++ vector1.py
@@ -0,0 +1,30 @@
+__author__ = 'student'
+class vector():
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
+        self.colour='violet'
+
+    def __lt__(self, other):
+        return self.x < other.x or self.x == other.x and self.y < other.y
+
+    def __gt__(self, other):
+        return self.x > other.x or self.x == other.x and self.y > other.y
+
+    def __add__(self, other):
+        return vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return vector(self.x - other.x, self.y - other.y)
+
+    def length(self):
+        l=(self.x**2 + self.y**2)**(1/2)
+        return l
+
+    def __str__(self):
+        print (self.x,' ', self.y)
+
+x, y=list(map(int, input().split(',')))
+a=vector(x, y)
+print (a.length())
+
